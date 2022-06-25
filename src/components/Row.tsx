77f83e5f46311e5e73cbf7409f02ad0714ba7faf
Yomna Osama <yabufrikha@gmail.com>
2022-06-25 23:26:48 +0200

    Added Row component

---
+++ src/components/Row.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components/native'
+import colors from '../constants/colors'
+import { perfectHeight, screenWidth } from '../services/commonFunctions'
+
+export const Row: React.FC = ({ children, ...props }) => {
+  return (
+    <Box {...props}>
+      {children}
+    </Box>
+  )
+}
+
+const Box = styled.View`
+  flex-direction: row;
+  width: ${p => (p.width || screenWidth * 0.9)}px;
+  height: ${perfectHeight(83)}px;
+  align-self: center;
+  background-color: ${colors.white};
+  justify-content: ${p => (p.justifyContent || 'space-between')};
+  align-items: center;
+`
